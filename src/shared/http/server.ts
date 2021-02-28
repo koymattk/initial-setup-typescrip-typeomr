d3fbcf5e752a4e6b254eeaca4644f41822b35f5e
Gabriel <koymattk@gmail.com>
2021-02-28 12:02:24 -0300

    addition class erro custom

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,26 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  } else {
+    return res.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  }
+});
+
 app.listen(3000, () => console.log('server running on port 3000! '));
